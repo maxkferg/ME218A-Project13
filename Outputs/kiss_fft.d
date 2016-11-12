7821128b0738091a78c749cbeeb683032c8e859b
Max Kristian Ferguson <maxferg@stanford.edu>
2016-11-11 18:38:46 -0800

    Removed backup files

--- Outputs/kiss_fft.d
+++ Outputs/kiss_fft.d
@@ -1,9 +1,8 @@
 .\outputs\kiss_fft.o: KissFourier\kiss_fft.c
-.\outputs\kiss_fft.o: .\Headers\_kiss_fft_guts.h
-.\outputs\kiss_fft.o: .\Headers\kiss_fft.h
+.\outputs\kiss_fft.o: KissFourier\kiss_fft/_kiss_fft_guts.h
+.\outputs\kiss_fft.o: KissFourier\kiss_fft/kiss_fft.h
 .\outputs\kiss_fft.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\stdlib.h
 .\outputs\kiss_fft.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\stdio.h
 .\outputs\kiss_fft.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\math.h
 .\outputs\kiss_fft.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\string.h
 .\outputs\kiss_fft.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\limits.h
-.\outputs\kiss_fft.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\alloca.h
